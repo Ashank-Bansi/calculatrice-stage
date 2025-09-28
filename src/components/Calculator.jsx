b72b24706999da3237f5fdaa7df915a14f73cd7b
Ashank-Bansi <bansiashank2006@gmail.com>
2025-09-28 18:43:25 -0400

    Base du code calculatrice

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import './Calculator.css';
+
+const buttons = [
+  '7', '8', '9', '/',
+  '4', '5', '6', '*',
+  '1', '2', '3', '-',
+  '0', '.', '=', '+',
+  'C'
+];
+
+export default function Calculator() {
+  return (
+    <div className="calculator">
+
+    </div>
+  );
+}
